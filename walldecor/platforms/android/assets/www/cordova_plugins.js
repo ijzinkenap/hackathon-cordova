18bfb1dbec99274cdaab16e76ab68ad43ec70b4b
Miguel Pereira <miguelsalespereira@gmail.com>
2014-06-26 13:22:44 +0200

    Installed Cordova Camera Plugin

--- walldecor/platforms/android/assets/www/cordova_plugins.js
+++ walldecor/platforms/android/assets/www/cordova_plugins.js
@@ -6,12 +6,41 @@ module.exports = [
         "clobbers": [
             "FileChooser"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
+        "id": "org.apache.cordova.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.cesidiodibenedetto.filechooser": "1.0.0"
+    "com.cesidiodibenedetto.filechooser": "1.0.0",
+    "org.apache.cordova.camera": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
